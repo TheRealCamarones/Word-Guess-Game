a76f8075b6f40809b2e296216521bf996cc5a98f
Camarones <cameron_walther@yahoo.com>
2019-02-06 20:48:49 -0500

    big shout out to Dom for asking me the right questions to really get me diagnosing what's going wrong here, everything works now!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,11 @@
 // Refer to the DOM elements I plan on updating as game is running
 var lettersGuessed = document.getElementById("letters-guessed");
-var remainingGuesses = document.getElementById("remaining-guesses");
-var answerArray = document.getElementById("answer-array");
-var wins = document.getElementById("wins-text");
-var losses = document.getElementById("losses-text");
+var guessesLeft = document.getElementById("remaining-guesses");
+var answerDisplay = document.getElementById("answer-array");
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+
+var button = document.getElementById("new-game-button")
 // var newGameButton = document.getElementById("new-game-button");
 
 // Set my variables
@@ -25,71 +27,73 @@ var losses = 0;
 // After talking about it with Dominick I agreed that ten is a good static number for 26 letters to guess, especially once you factor
 // out x, z, q and other rarely used letters
 var remainingGuesses = 10;
-var gameRunning = true;
+var gameRunning = false;
 var answer = "";
 var answerArray = [];
-var guessedLetter = [""];
 var incorrectLetter = [];
-
-// Pick a random word from my options in the array
-var word = words[Math.floor(Math.random() * words.length)];
-
-// Make an array of blank spaces to display to the user while they guess
-
-
+var word = "";
+var letter = "";
 
 // newGame function will reset stats
 function newGame() {
     gameRunning = true;
     remainingGuesses = 10;
-    lettersGuessed = [""];
+    
     incorrectLetter = [];
-    answerArray = [""];
+    answerArray = [];
+    //Pick a random word from my options in the array declare it in the global scope and then reassign inside the function...i think?
+    word = words[Math.floor(Math.random() * words.length)];
+
+    // Make an array of blank spaces to display to the user while they guess
     for (var i = 0; i < word.length; i++) {
         answerArray.push("_");
     }
+    console.log(typeof answerArray.join(" "));
+    console.log(word);
+    console.log(answerDisplay);
+    
 
     // Add game info to DOM
-    lettersGuessed.textContent = lettersGuessed;
-    remainingGuesses.textContent = remainingGuesses;
-    answerArray.textContent = answerArray;
-    wins.textContent = wins;
-    losses.textContent = losses;
+    lettersGuessed.textContent = incorrectLetter.join(" ");
+    guessesLeft.textContent = remainingGuesses;
+    answerDisplay.textContent = answerArray.join(" ");
+    winsText.textContent = wins;
+    lossesText.textContent = losses;
+    button.textContent = "Guess a letter to play"
+    
 }
 
 function guess(letter) {
-    console.log(letter);
-    console.log(word)
     // checks that the game is running and that the letter hasn't been guessed already
-    if (gameRunning === true && lettersGuessed.indexOf(letter) === -1) {
-        // and then runs the game logic
-        lettersGuessed.push(letter);
+    if (gameRunning === true && incorrectLetter.indexOf(letter) === -1) {
+        // // and then runs the game logic
+        // incorrectLetter.push(letter);
 
         // check if the guess is correct
-        for (var j = 0; j < remainingGuesses; j++) {
+        for (var j = 0; j < word.length; j++) {
             if (word[j].toLowerCase() === letter.toLowerCase()) {
-                answerArray[j] === word[j];
+                answerArray[j] = word[j];
+                // gameWin = j
             }
         }
-        answerArray.textContent = answerArray.join("");
-    }
-    else {
-        if (!gameRunning) {
-            alert("Start a New Game");
-        } else {
-            alert("You've already guessed this letter, try a new one!");
+        answerDisplay.textContent = answerArray.join(" ");
         }
-    }
-}
+        else {
+                alert("You've already guessed this letter, try a new one!");
+            }
+        }
+
 // Check for incorrect letter
-function checkincorrect(letter) {
+function checkIncorrect(letter) {
     if (answerArray.indexOf(letter.toLowerCase()) === -1
         &&
-        answerArray.indexOf(letter.toUpperCase()) === -1) {
+        answerArray.indexOf(letter.toUpperCase()) === -1
+        &&
+        incorrectLetter.indexOf(letter) === -1) {
         remainingGuesses--;
         incorrectLetter.push(letter);
-        lettersGuessed.textContent = incorrectLetter.join("");
-        remainingGuesses.textContent = remainingGuesses;
+        lettersGuessed.textContent = incorrectLetter.join(" ");
+        guessesLeft.textContent = remainingGuesses;
     }
 }
 
@@ -98,26 +102,33 @@ function checkLoss() {
     if (remainingGuesses === 0) {
         losses++;
         gameRunning = false;
-        losses.textContent = losses;
+        lossesText.textContent = losses;
+        button.textContent = "Sorry, you lost. Press any key to play again!";
     }
     checkWin();
 }
 
 // Check win
 function checkWin() {
-    if (word.toLowerCase() === answerArray.join("").toLowerCase()) {
+    if (word.toLowerCase() === answerArray.join("")) {
         wins++;
         gameRunning = false;
-        wins.textContent = wins;
+        winsText.textContent = wins;
+        button.textContent = "Conratulations! You know a lot of types of beer!"
     }
 }
-// click event to start the game
-// newGameButton.addEventListener("click", newGame);
 
 // key event to guess a letter that's actually a letter
 document.onkeyup = function (event) {
     if (event.keyCode >= 65 && event.keyCode <= 90) {
-        newGame();
+        letter = event.key.toLowerCase();
+        if (gameRunning === false) {
+            newGame();
+        }
+        guess(letter);
+        checkIncorrect(letter);
+        checkLoss(letter);
         console.log(event.key);
+        console.log(lettersGuessed);
     }
 }
